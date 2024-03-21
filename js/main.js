018dd5fd04d8ecdef01c0693c1ada5432d8bc4b9
Andrii Nyzhnyk <prostoandrew1994@gmail.com>
2024-03-21 19:50:28 +0200

    added features section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
-$(function(){
+'use strict'
+const tabItem = document.querySelectorAll('.tabs__btn-item')
+const tabContent = document.querySelectorAll('.tabs__content-item')
 
-});
+
+function open(event) {
+    const tabTarget = event.currentTarget;
+    const button = tabTarget.dataset.button;
+
+    tabItem.forEach( function(el) {
+        el.classList.remove('tabs__btn-item--active')
+    } )
+    tabContent.forEach( function(el) {
+        el.classList.remove('tabs__content-item--active')
+    } )
+
+    tabTarget.classList.add('tabs__content-item--active')
+    document.querySelector(`#${button}`).classList.add('tabs__content-item--active')
+}
+
+
+tabItem.forEach( function(el) {
+    el.addEventListener('click', open)
+} )
